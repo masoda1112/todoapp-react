baaa10fb4a24cba928128b6972d5f91422e294a7
masoda1112 <wendy.odakura@gmail.com>
2021-03-22 18:37:25 +0900

    provider、useContextを使用して実装完了

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import TodoList from '../src/components/todoList'
+import TodoList from '../src/components/todoList'
 import './App.css';
 import {createContext, useReducer, useState} from 'react';
 import reducer from './stores/reducer';
@@ -14,39 +14,12 @@ function App() {
 
   const [task, setTask] = useState('')
   const [state, dispatch] = useReducer(reducer, initialState)
-  const handleNewTask = (event) => {
-    setTask( event.target.value )
-  }
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    // フォームがからの時にタスクに追加しない
-    if(task === '') return
-    // todoに入力した文字が追加
-    dispatch({type: 'POST', task})
-    console.log({state})
-    // フォームに表示されている内容が削除
-    setTask('')
-  }
+  
 
   return (
-    <>
-      <div>
-        <h1>ToDo List</h1>
-        <form onSubmit={ handleSubmit }>
-          Add Task : <input value={ task } placeholder="Add New Task" onChange={handleNewTask}/>
-        </form>
-        <ul>
-          { state.map((t, index) => (
-          <li 
-            key={ index } 
-          >
-            { t.task }
-          </li>
-          ))
-          }
-        </ul>
-      </div>
-    </>
+      <Context.Provider value={{state, dispatch}}>
+        <TodoList />
+      </Context.Provider>
   );
 }
 
--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, {useContext, useState} from 'react';
 import {Context} from '../App';
 // import {reducer} from '../stores/reducer';
 
@@ -14,33 +14,33 @@ const TodoList = () => {
       if(task === '') return
       // todoに入力した文字が追加
       // setTodo(todos => [...todos,{ task, isCompleted: false}])
-      Context.dispatch({type: 'POST', task})
+      value.dispatch({type: 'POST', task})
       // フォームに表示されている内容が削除
+      console.log({value})
       setTask('')
     }
+    const value = useContext(Context);
     return (
-      <Context.Consumer>
-        { ({state}) => (
-          <div>
-            <h1>ToDo List</h1>
-            <form onSubmit={ handleSubmit }>
-              Add Task : <input value={ task } placeholder="Add New Task" onChange={handleNewTask}/>
-            </form>
-            <ul>
-              { state.map((t, index) => (
+      <>
+        <div>
+          <h1>ToDo List</h1>
+          <form onSubmit={ handleSubmit }>
+            Add Task : <input value={ task } placeholder="Add New Task" onChange={handleNewTask}/>
+          </form>
+          <ul>
+            { value.state.map((t, index) => (
               <li 
                 key={ index } 
               >
                 { t.task }
               </li>
-              ))
-              }
-            </ul>
-          </div>
-          )
-        }
-      </Context.Consumer>
+            ))
+            }
+          </ul>
+        </div>
+      </>
     );
 }
 
 export default TodoList;
+
